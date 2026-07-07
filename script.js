02f481ba55266ee2bf3c0ee91e085230ddf5bde3
Ayush Bisen <ayushbisen321@gmail.com>
2026-07-07 13:30:59 +0530

    intial

---
+++ script.js
@@ -0,0 +1,9 @@
+function greet() {
+    alert("Welcome to javascript");
+    console.table([{
+        Name: "sample",
+        Course: "javascript"
+    }]);
+    console.time("Execution");
+    console.timeEnd("Execution");
+}
